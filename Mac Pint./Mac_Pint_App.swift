ec1f75a137187429c90fcf636794e45a76f4d299
shahriarRahmanShaon <shaon463@gmail.com>
2021-05-05 13:51:50 +0600

    Initial Commit

---
+++ Mac Pint./Mac_Pint_App.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_Pint_App.swift
+//  Mac Pint.
+//
+//  Created by sergio shaon on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Mac_Pint_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
